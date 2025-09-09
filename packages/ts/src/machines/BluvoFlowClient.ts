353765b6f0a04620deea31ba2e22f74325e46719
flo <sabaniflorian@gmail.com>
2025-09-09 18:46:03 -0400

    quotation has amounts in USD as well

--- packages/ts/src/machines/BluvoFlowClient.ts
+++ packages/ts/src/machines/BluvoFlowClient.ts
@@ -200,7 +200,6 @@ export class BluvoFlowClient {
         balances: withdrawableBalanceInfo.balances.map(b => ({
           asset: b.asset,
           balance: String(b.amount),
-          balanceInFiat: "ciao",
           networks: b.networks,
 
           // if amountInFiat is present (including 0), include balanceInFiat

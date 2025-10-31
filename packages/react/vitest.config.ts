5e71ee38c237f9cf2b308c3513d62cf50baeabea
flo <sabaniflorian@gmail.com>
2025-10-31 13:10:56 -0400

    v2.0.7 remove lazy import - attempt fix tests for GHAction

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import * as path from 'path';
 
 export default defineConfig({
   test: {
     environment: 'node',
     globals: true
+  },
+  resolve: {
+    alias: {
+      '@bluvo/sdk-ts': path.resolve(__dirname, '../ts/index.ts')
+    }
   }
 });
